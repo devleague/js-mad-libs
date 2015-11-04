b6a75a03b20ac50946b23cb07e43b95f9230026f
senakamura <seanenakamura@gmail.com>
2015-11-03 18:28:16 -1000

    randomizes three arrays

--- madlibs.js
+++ madlibs.js
@@ -11,20 +11,22 @@ window.onload = function (){
   var blankAdj = document.getElementsByClassName('adj');
   var blankVerb = document.getElementsByClassName('verb');
 
-  //Random Noun
-  for(var n = 0; n < blankNoun.length; n++){
-    blankNoun[n].innerHTML = randomWord(nouns);
-  }
+  document.getElementById('randomize').onclick = function(){
+    //Random Noun
+    for(var n = 0; n < blankNoun.length; n++){
+      blankNoun[n].innerHTML = randomWord(nouns);
+    }
 
-  //Random Adjective
-  for(var a = 0; a < blankAdj.length; a++){
-    blankAdj[a].innerHTML = randomWord(adjectives);
-  }
+    //Random Adjective
+    for(var a = 0; a < blankAdj.length; a++){
+      blankAdj[a].innerHTML = randomWord(adjectives);
+    }
 
-  //Random Verb
-  for(var v = 0; v < blankAdj.length; v++){
-    blankVerb[v].innerHTML = randomWord(verbs);
-  }
+    //Random Verb
+    for(var v = 0; v < blankAdj.length; v++){
+      blankVerb[v].innerHTML = randomWord(verbs);
+    }
+  };
 
   function randomWord (arr){
     var num = Math.floor(Math.random() * (arr.length - 0));
