0cf882e3c030ac8411b810c1e50de54cdb375112
senakamura <seanenakamura@gmail.com>
2015-11-03 17:47:31 -1000

    searches for classes, randomizes which to append to, randomizes words to choose from array

--- madlibs.js
+++ madlibs.js
@@ -1,5 +1,34 @@
 // Write a list of nouns, verbs, and adjectives in their arrays.
 
-var noun = [];
-var verb = [];
-var adjective = [];
+var nouns = ['bob', 'rabbit', 'apple pie', 'stormtrooper'];
+var verbs = ['munch', 'run', 'jump', 'cook'];
+var adjectives = ['delicious', 'purple', 'crusty', 'sour'];
+
+
+window.onload = function (){
+
+  var blankNoun = document.getElementsByClassName('noun');
+  var blankAdj = document.getElementsByClassName('adj');
+  var blankVerb = document.getElementsByClassName('verb');
+
+  //Random Noun
+  for(var n = 0; n < blankNoun.length; n++){
+    blankNoun[n].innerHTML = randomWord(nouns);
+  }
+
+  //Random Adjective
+  for(var a = 0; a < blankAdj.length; a++){
+    blankAdj[a].innerHTML = randomWord(adjectives);
+  }
+
+  //Random Verb
+  for(var v = 0; v < blankAdj.length; v++){
+    blankVerb[v].innerHTML = randomWord(verbs);
+  }
+
+  function randomWord (arr){
+    var num = Math.floor(Math.random() * (arr.length - 0));
+    return arr[num];
+  }
+
+};
