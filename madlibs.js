d8204633bd8695b5b4594bc967ba3065e734fbbc
senakamura <seanenakamura@gmail.com>
2015-11-03 20:44:01 -1000

    moved functions out. left arrays for testing

--- madlibs.js
+++ madlibs.js
@@ -1,40 +1,9 @@
-// Write a list of nouns, verbs, and adjectives in their arrays.
+/* Create three arrays called nouns, verbs, and adjectives
+ *
+ * In each array write at least 5 to 10 values that describe its
+ * parts of speech. ex. chicken is a noun.
+ */
 
-var nouns = ['bob', 'chicken', 'robot', 'Tokyo'];
-var verbs = ['munch', 'run', 'jump', 'cook'];
-var adjectives = ['delicious', 'purple', 'crusty', 'sour'];
-
-
-window.onload = run;
-
-function run(){
-
-  var blankNoun = document.getElementsByClassName('noun');
-  var blankAdj = document.getElementsByClassName('adj');
-  var blankVerb = document.getElementsByClassName('verb');
-
-  document.getElementById('randomize').onclick = randomize;
-
-  function randomWord (arr){
-    var num = Math.floor(Math.random() * (arr.length));
-    return arr[num];
-  }
-
-  function randomize (){
-    //Random Noun
-    for(var n = 0; n < blankNoun.length; n++){
-      blankNoun[n].innerHTML = randomWord(nouns);
-    }
-
-    //Random Adjective
-    for(var a = 0; a < blankAdj.length; a++){
-      blankAdj[a].innerHTML = randomWord(adjectives);
-    }
-
-    //Random Verb
-    for(var v = 0; v < blankAdj.length; v++){
-      blankVerb[v].innerHTML = randomWord(verbs);
-    }
-  }
-
-}
+var nouns = ['bob', 'chicken', 'robot', 'park', 'dragon', 'chef', 'tree', 'surfboard'];
+var verbs = ['munch', 'run', 'jump', 'cook', 'surf', 'smack', 'game', 'hop'];
+var adjectives = ['delicious', 'purple', 'crusty', 'sour', 'wet', 'sticky', 'scalding'];
