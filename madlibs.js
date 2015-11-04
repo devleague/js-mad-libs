44a2648ca3cd8bbfb57135fb4b83f1c61482ef8c
senakamura <seanenakamura@gmail.com>
2015-11-03 22:56:43 -1000

    removes pre-made arrays. clean slate for students to use

--- madlibs.js
+++ madlibs.js
@@ -3,7 +3,3 @@
  * In each array write at least 5 to 10 values that describe its
  * parts of speech. ex. chicken is a noun.
  */
-
-var nouns = ['bob', 'chicken', 'robot', 'park', 'dragon', 'chef', 'tree', 'surfboard'];
-var verbs = ['munch', 'run', 'jump', 'cook', 'surf', 'smack', 'game', 'hop'];
-var adjectives = ['delicious', 'purple', 'crusty', 'sour', 'wet', 'sticky', 'scalding'];
