63969c53de1d311df93bf121ad4360f21605e01f
senakamura <seanenakamura@gmail.com>
2015-11-03 19:55:00 -1000

    moves functions around for organization

--- madlibs.js
+++ madlibs.js
@@ -1,17 +1,26 @@
 // Write a list of nouns, verbs, and adjectives in their arrays.
 
-var nouns = ['bob', 'rabbit', 'apple pie', 'stormtrooper'];
+var nouns = ['bob', 'chicken', 'robot', 'Tokyo'];
 var verbs = ['munch', 'run', 'jump', 'cook'];
 var adjectives = ['delicious', 'purple', 'crusty', 'sour'];
 
 
-window.onload = function (){
+window.onload = run;
+
+function run(){
 
   var blankNoun = document.getElementsByClassName('noun');
   var blankAdj = document.getElementsByClassName('adj');
   var blankVerb = document.getElementsByClassName('verb');
 
-  document.getElementById('randomize').onclick = function(){
+  document.getElementById('randomize').onclick = randomize;
+
+  function randomWord (arr){
+    var num = Math.floor(Math.random() * (arr.length));
+    return arr[num];
+  }
+
+  function randomize (){
     //Random Noun
     for(var n = 0; n < blankNoun.length; n++){
       blankNoun[n].innerHTML = randomWord(nouns);
@@ -26,11 +35,6 @@ window.onload = function (){
     for(var v = 0; v < blankAdj.length; v++){
       blankVerb[v].innerHTML = randomWord(verbs);
     }
-  };
-
-  function randomWord (arr){
-    var num = Math.floor(Math.random() * (arr.length - 0));
-    return arr[num];
   }
 
-};
+}
