1303fc412a3e6111298502b22800cf443146243c
senakamura <seanenakamura@gmail.com>
2015-11-04 02:00:14 -1000

    reorganized madlibs.js to work with tests

--- madlibs.js
+++ madlibs.js
@@ -3,3 +3,7 @@
  * In each array write at least 5 to 10 values that describe its
  * parts of speech. ex. chicken is a noun.
  */
+
+ var nouns = ['foo', 'lol'];
+ var adjectives = ['bar'];
+ var verbs = ['baz'];
